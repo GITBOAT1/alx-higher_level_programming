b10189385501ebd7fef87e63f20393aafd862025
joe <boat232001@yahoo.com>
2021-11-17 17:46:01 -0800

    Shell

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-def Lcase():
-    for i in range(97, 123):
-        print(chr(i), end="")
-
-
-Lcase()
+for i in range(97, 123):
+    print(chr(i), end="")
--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
-
-def Hex():
-    for i in range(0, 99):
-        print("{:d} = 0x{:0x}".format(i, i))
-
-
-Hex()
+for i in range(0, 99):
+    print("{:d} = 0x{:0x}".format(i, i))
