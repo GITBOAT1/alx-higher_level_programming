bf14a60c06e93f5618eafc76ad7145d3f3fe5dd4
joe <boat232001@yahoo.com>
2021-11-20 17:52:03 -0800

    replace

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Replace element
+"""
+
+
+def replace_in_list(my_list, idx, element):
+    if idx < 0 or idx > len(my_list):
+        return(my_list)
+    else:
+        my_list[idx] = element
+        return(my_list)
