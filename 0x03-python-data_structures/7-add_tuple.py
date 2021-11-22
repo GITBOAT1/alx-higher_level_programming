4077ea719624b210029960bae53c918c697fa71b
joe <boat232001@yahoo.com>
2021-11-22 10:33:53 -0800

    adding

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Tupple Adding
+"""
+
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    ze = (0,)
+
+    while(len(tuple_a) < 2):
+        tuple_a += ze
+    while(len(tuple_b) < 2):
+        tuple_b += ze
+    tup = list(tuple_a)
+    tup1 = list(tuple_b)
+    for i in range(0, (len(tuple_a))):
+        tup[i] = (tup[i] + tup1[i])
+    ze = tuple(tup)
+    return(ze)
