214a5dc66578d316a1ece17449841403104bf486
joe <boat232001@yahoo.com>
2021-11-23 17:05:22 -0800

    1 elemet update

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -4,6 +4,8 @@
 
 
 def print_reversed_list_integer(my_list=[]):
+    if len(my_list) == 0:
+        return(None)
     a = len(my_list) - 1
     for i in range(len(my_list)):
         print("{:d}".format(my_list[a]))
