954427fabcb7452bf6c798cbd1328ae4b3cc8d7e
joe <boat232001@yahoo.com>
2021-11-24 10:38:15 -0800

    matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -4,11 +4,12 @@
 
 
 def square_matrix_simple(matrix=[]):
-    mx1 = []
     mx2 = []
     count = 0
     for i in matrix:
+        mx1 = []
         for a in range(0, len(i)):
             mx1.append(i[a] * i[a])
-    mx2.append(mx1)
+        mx2.insert(count, mx1)
+        count += 1
     return(mx2)
