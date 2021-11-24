106fc5b0cb1710d726f8f65cf42a843fb4d30a25
joe <boat232001@yahoo.com>
2021-11-23 17:25:57 -0800

    3element

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -4,7 +4,7 @@
 
 
 def delete_at(my_list=[], idx=0):
-    if idx < 0 or idx > len(my_list):
+    if idx < 0 or idx > (len(my_list)) - 1:
         return(my_list)
     else:
         del my_list[idx]
