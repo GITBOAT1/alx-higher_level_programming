f1c5f3080590cbf42ce02270241978ed3dc243ed
joe <boat232001@yahoo.com>
2021-12-07 15:52:41 -0800

    docs

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -3,5 +3,10 @@
 
 
 class Square:
+    """Example of docstring on the __init__ method.
+
+        The __init__ method may be documented in either the class level
+        docstring, or as a docstring on the __init__ method itself.
+    """
     def __init__(self):
         pass
