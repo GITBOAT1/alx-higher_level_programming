decd9d69d736cfd723d0d8ab36646d90af6af203
joe <boat232001@yahoo.com>
2021-12-07 16:24:46 -0800

    Square size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Square with size"""
+
+
+class Square:
+    """ The size of a square is crucial for a square, many things depend of it
+    (area computation, etc.), so you, as class builder, must control the type
+    and value of this attribute. One way to have the control is to keep it
+    privately. You will see in next tasks how to get, update and validate the
+    size value.
+
+    Args:
+        size(int): this tells the size of the figure
+    """
+    def __init__(self, size):
+        pass
