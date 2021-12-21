966f6a67c6bc882e14c9d172cc4c3230f73c7446
GITBOAT <boat232001@yahoo.com>
2021-12-20 16:50:41 -0800

    01

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+A function that returns the list of
+available attributes and methods of an object
+
+"""
+
+
+def lookup(obj):
+    """ availabe names for attributes and methods """
+    return(dir(obj))
