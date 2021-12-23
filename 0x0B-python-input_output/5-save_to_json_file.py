5d6520a6d8ae90440032cd339dbf9516b5879adc
GITBOAT <boat232001@yahoo.com>
2021-12-22 17:03:03 -0800

    json write

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Save Object to a file
+"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """
+    a function that writes an Object to a
+    text file, using a JSON representation
+    """
+    with open(filename, 'w') as f:
+        f.write(json.dumps(my_obj))
+
