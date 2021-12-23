7d3f66305994dad04920ea6ec8cfd7080cfb0731
GITBOAT <boat232001@yahoo.com>
2021-12-22 18:11:41 -0800

    student class

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Student to disk and reload
+"""
+
+
+class Student:
+    """
+    a class Student that defines a student by: (based on 10-student.py)
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """initalizer
+        Args
+           first_name
+           last_name
+           age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves dictionary representation of instance"""
+        if not attrs:
+            return (self.__dict__)
+        if not isinstance(attrs, list):
+            return (self.__dict__)
+        if not (isinstance(attrs[elem], str) for elem in attrs):
+            return (self.__dict__)
+        return ({key: value for key, value in self.__dict__.items()
+                 if key in attrs})
+
+    def reload_from_json(self, json):
+        for key, value in json.items():
+            setattr(self, key, value)
