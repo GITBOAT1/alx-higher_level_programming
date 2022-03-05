b89041fee7429772e2a799a3aed045123da89a11
GITBOAT <boat232001@yahoo.com>
2022-03-04 17:16:47 -0800

    0-3

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Script that lists all states from the database hbtn_0e_0_usa """
+import MySQLdb
+import sys
+
+
+def host():
+    """ this is where my sql starts """
+    MY_HOST = "localhost"
+
+    db = MySQLdb.connect(host=MY_HOST, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    name = sys.argv[4]
+    cur.execute("SELECT * FROM states WHERE name = '%s'" % name)
+
+    for i in cur:
+        """ print all the valuse in states """
+        print(i)
+
+        """ close the data base """
+        cur.close()
+        db.close()
+
+
+if __name__ == "__main__":
+    host()
---
+++ 0x0F-python-object_relational_mapping/README.md
@@ -0,0 +1,3 @@
+# Python - Object-relational mapping
+** The biggest difference is: no more SQL queries! Indeed, the purpose of an ORM is to abstract the storage to the usage. With an ORM, your biggest concern will be “What can I do with my objects” and not “How this object is stored? where? when?”. You won’t write any SQL queries only Python code. Last thing, your code won’t be “storage type” dependent. You will be able to change your storage easily without re-writing your entire project.
+
