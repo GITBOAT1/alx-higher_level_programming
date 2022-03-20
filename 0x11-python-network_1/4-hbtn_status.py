f5d113cdd361a8de426c88816dfe32a30df59b36
GITBOAT <boat232001@yahoo.com>
2022-03-20 10:11:51 -0700

    246

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -2,7 +2,7 @@
 """ What's my status? #1 """
 import requests
 
-r = requests.get('https://intranet.hbtn.io/status')
+r = requests.get('https://alx-intranet.hbtn.io/status')
 print("Body response:")
 print("\t- type: {}".format(type(r.text)))
 print("\t- content: {}".format(r.text))
