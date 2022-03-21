15d27a12eafaa8a4db1cdcbffda587ef7a4873da
GITBOAT <boat232001@yahoo.com>
2022-03-20 19:21:44 -0700

    05

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Bash script that takes in a URL, sends a request to
-# that URL, and displays the size of the body of the response
-curl -sI $1 | grep 'Content-Length' | cut -f2 -d ':'
+# Bash script that takes in a URL, sends POST request to the passed URl
+curl -sd "test@gmail.com&I will always be here for PLD" $1 
