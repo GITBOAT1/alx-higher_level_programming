f465e2445383464023dc985ad4271f5efde60174
GITBOAT <boat232001@yahoo.com>
2022-03-20 19:33:10 -0700

    05

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Bash script that takes in a URL, sends POST request to the passed URl
-curl -sd "test@gmail.com&I will always be here for PLD" $1 
+# sends POST request to the passed URl
+curl -sd "email=test@gmail.com&subject=I will always be here for PLD" $1 
