4b98f5985452bf22fa77227d680eb9dd826b2b2b
Joe <boat232001@yahoo.com>
2023-01-02 13:51:08 +1000

    Linked list cycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -2,24 +2,26 @@
 
 /**
  * check_cycle - This is just a simple func to tell there is a cycle
- * @list: this holds the valuse from the head node.
+ * @list: this holds the values from the head node.
  * Return: return 0 if not a cycle
 */
 
 int check_cycle(listint_t *list)
 {
-	listint_t *tmp = list;
-	listint_t *cur = list->next;
+    listint_t *tmp = list;
+    listint_t *cur = list->next;
 
-	if (!list || list->next)
-		return (0);
+    if (!list || !list->next)
+    return (0);
 
-	while (cur && cur->next)
+    while (cur && cur->next)
+    {
+	cur = cur->next->next;
+	tmp = tmp->next;
+	if (tmp == cur)
 	{
-		if (tmp == cur)
-			return (1);
-		tmp = tmp->next;
-		cur = cur->next->next;
+	     return (1);
 	}
-	return (0);
+    }
+    return (0);
 }
