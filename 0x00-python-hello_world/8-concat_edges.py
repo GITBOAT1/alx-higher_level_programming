fc06991b62db72b8d6042de7a514e4a9f6239e69
Joe <boat232001@yahoo.com>
2023-01-02 12:18:50 +1000

    Create a new sentence mandatory

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = str[40:-62] + str[107:112] + str[0:7]
+str = str[39:-62] + str[107:112] + str[0:7]
 print(str)
