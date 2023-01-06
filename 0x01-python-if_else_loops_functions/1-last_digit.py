df4ce9c1e8d592043673745921a3457491244fe6
Joe <boat232001@yahoo.com>
2023-01-06 22:17:22 +1000

    The last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,14 +1,23 @@
 #!/usr/bin/python3
+""" Program to check if  last digit is >5 or <5 or == 0 """
 import random
 number = random.randint(-10000, 10000)
 
 la = int(str(number)[-1:])
 s = "Last digit of"
-if int(la) > 5:
-    print("{} {:d} is {:d} and is greater than 5".format(s, number, la))
-elif int(la) == 0:
-    print("{} {:d} is {:d} and is 0".format(s, number, la))
-elif int(la) < 6 and int(la) != 0:
-    if number < 0:
-        la = (-1) * la
+
+
+""" check if its nagative """
+if (number < 0):
+    la = (-1) * la
     print("{} {:d} is {:d} and is less than 6 and not 0".format(s, number, la))
+else:
+    if int(la) > 5:    
+        print("{} {:d} is {:d} and is greater than 5".format(s, number, la))
+    elif int(la) == 0:
+        print("{} {:d} is {:d} and is 0".format(s, number, la))
+    elif int(la) < 6 and int(la) != 0:
+        print("{} {:d} is {:d} and is less than 6 and not 0".format(s, number, la))
+
+
+
