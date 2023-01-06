45419402a9a69588c64013c646a0d6a3f59a6dd6
Joe <boat232001@yahoo.com>
2023-01-06 22:54:34 +1000

    The last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,19 +5,18 @@ number = random.randint(-10000, 10000)
 
 la = int(str(number)[-1:])
 s = "Last digit of"
-
+strn = "and is less than 6 and not 0"
 
 """ check if its nagative """
 if (number < 0):
     la = (-1) * la
     print("{} {:d} is {:d} and is less than 6 and not 0".format(s, number, la))
 else:
-    if int(la) > 5:    
+    if int(la) > 5:
         print("{} {:d} is {:d} and is greater than 5".format(s, number, la))
     elif int(la) == 0:
         print("{} {:d} is {:d} and is 0".format(s, number, la))
     elif int(la) < 6 and int(la) != 0:
-        print("{} {:d} is {:d} and is less than 6 and not 0".format(s, number, la))
-
-
-
+        print("{} {:d} is {:d} {}".format(s, number, la, strn))
+    elif (number != int):
+        print("TypeError")
