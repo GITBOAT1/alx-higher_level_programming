b5b9cf72af6bb9a8a5b1ae674f18c57cf1c4531b
Joe <boat232001@yahoo.com>
2023-01-08 21:11:36 +1000

    dictionary

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -9,4 +9,4 @@ def simple_delete(a_dictionary, key=""):
     if a_dictionary.get(key):
         del a_dictionary[key]
 
-    return(a_dictionary)
+    return (a_dictionary)
--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -4,11 +4,13 @@
 
 
 def multiply_by_2(a_dictionary):
-    """ function that returns a new dictionary with all values multiplied by 2"""
+    """ function that returns a new dictionary
+        with all values multiplied by 2
+    """
 
     new_dictionary = dict()
 
     for key in a_dictionary:
         new_dictionary[key] = a_dictionary[key] * 2
 
-    return(new_dictionary)
+    return (new_dictionary)
