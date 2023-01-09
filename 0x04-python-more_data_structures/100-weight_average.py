89941150e69c710b0d0656bbf2070cbc06ed8ec7
Joe <boat232001@yahoo.com>
2023-01-09 22:18:12 +1000

    delete by value

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -9,7 +9,7 @@ def weight_average(my_list=[]):
     """
 
        if my_list:
-           res = sum(a * b for a, b in my_list) / sum(b for a, b in my_list)
+           res = sum(a * b for a, b in my_list) / sum(b for a, b in my_list))
            return (res)
 
     return(0)
