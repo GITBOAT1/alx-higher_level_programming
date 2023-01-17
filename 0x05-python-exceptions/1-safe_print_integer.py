864ce98f0e1f887dae95727b8734ed7123955833
Joe <boat232001@yahoo.com>
2023-01-17 21:08:38 +1000

    NameError

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -6,5 +6,5 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError:
-        return False       
+    except ValueError, TypeError:
+        return False
