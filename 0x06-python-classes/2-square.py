81a2b75da3e9569e19e60ec6873a8214edf2ebb4
Joe <boat232001@yahoo.com>
2023-02-06 20:28:54 +1000

    size Balidatio

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,19 +4,13 @@
 
 class Square:
     """ The size of a square is crucial for a square, many things depend of it
-    (area computation, etc.), so you, as class builder, must control the type
-    and value of this attribute. One way to have the control is to keep it
-    privately. You will see in next tasks how to get, update and validate the
-    size value.
-
-    Args:
-        size: this tells the size of the figure
-
-    Attributes:
-        size: Human readable string describing the exception.
     """
     __size = None
 
-    def __init__(self):
+    def __init__(self, size=0):
         """ init method """
         self.__size = size
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
