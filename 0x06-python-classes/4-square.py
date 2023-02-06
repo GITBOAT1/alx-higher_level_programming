75b6d2c46298956afe101cecef50dbfe25f7f4b9
Joe <boat232001@yahoo.com>
2023-02-06 21:48:55 +1000

    access private attribute

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Square with size"""
+
+
+class Square:
+    """ The size of a square is crucial for a square, many things depend of it
+    """
+    __size = None
+
+    def __init__(self, size=0):
+        """ init method """
+        self.__size = size
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return (self.__size * self.__size)
+
+    @property
+    def size(self):
+        """ property get to retrieve it """
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """ property setter to set """
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
