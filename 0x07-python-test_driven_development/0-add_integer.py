7586184c181342c7f0b1e6e6ed508d053256fb73
Joe <boat232001@yahoo.com>
2023-02-08 07:15:54 +1000

    add

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -4,17 +4,15 @@ Prototype: def add_integer(a, b=98):
     a function that adds 2 integers.
 """
 
-Err = " must be an interger"
-
 
 def add_integer(a=0, b=98):
     """
     This is the "example" module.
     """
-    
+
     if not isinstance(a, (int, float)):
-        raise TypeError('a' + Err)
+        raise TypeError("a must be an integer")
     elif not isinstance(b, (int, float)):
-        raise TypeError('b' + Err)
+        raise TypeError("b must be an integer")
     else:
         return (int(a) + int(b))
--- 0x07-python-test_driven_development/0-main.py
+++ 0x07-python-test_driven_development/0-main.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python3
+""" Doc """
+
 add_integer = __import__('0-add_integer').add_integer
 
-print(add_integer(1, 2))
-print(add_integer(100, -2))
-print(add_integer(2))
-print(add_integer(100.3, -2))
 try:
-    print(add_integer(4, "School"))
-except Exception as e:
-    print(e)
-try:
-    print(add_integer(None))
+    print(add_integer(1, "2"))
 except Exception as e:
     print(e)
