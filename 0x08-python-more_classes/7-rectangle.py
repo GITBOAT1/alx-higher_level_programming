a8f5c5d7722bc941529a5e65cbb262a34386b3ce
Joe <boat232001@yahoo.com>
2023-03-21 21:34:23 +1000

    number of instance

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -8,6 +8,9 @@ Rectangle that defines a rectangle by: (based on 0-rectangle.py)
 class Rectangle:
     """ An empty Rectangle """
 
+    number_of_instances = 0
+    print_symbol = '#'
+
     def __init__(self, width=0, height=0):
         """ instantiat width """
         self.height = height
@@ -67,13 +70,13 @@ class Rectangle:
         """prints rectangle"""
         if self.height == 0 or self.width == 0:
             return ''
-        hashes = '#' * self.width
+        hashes = "{}".format(self.print_symbol) * self.width
         return '\n'.join(hashes for i in range(self.height))
 
     def __repr__(self):
         """prints rectangle"""
         eval('Rectangle(self.width, self.height)')
-        return("Rectangle({}, {})".format(self.width, self.height))
+        return ("Rectangle({}, {})".format(self.width, self.height))
 
     def __del__(self):
         print("Bye rectangle...")
